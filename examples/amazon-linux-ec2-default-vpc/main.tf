a10e4befe66bda9767c42f5951735720e2d0bf0e
Rajiv Mandal <rajiv.mandal@intel.com>
2022-12-09 12:22:34 -0800

    add randon numbers to name of VM and owner name to create uniqueness

--- examples/amazon-linux-ec2-default-vpc/main.tf
+++ examples/amazon-linux-ec2-default-vpc/main.tf
@@ -1,9 +1,14 @@
 # Provision EC2 Instance on Icelake on Aamzon Linux OS in default vpc
+
+  resource "random_id" "rid" {
+    byte_length = 5
+  }
+
 module "ec2-vm" {
-  source         = "../../"
+  source = "../../"
   tags = {
-    Name = "my-test-vm"
-    Owner = "Rajiv M",
+    Name     = "my-test-vm-${random_id.rid.dec}"
+    Owner    = "OwnerName-${random_id.rid.dec}",
     Duration = "2"
   }
 }
