cac18afd5e4dbd547eb1ce7a58e7f488ba02d6c5
Rajiv Mandal <rajiv.mandal@intel.com>
2022-12-12 15:56:12 -0800

    add ec2 key pair and security group for ss in ec2 amazon linux default vpc example

--- examples/amazon-linux-ec2-default-vpc/main.tf
+++ examples/amazon-linux-ec2-default-vpc/main.tf
@@ -1,11 +1,50 @@
-# Provision EC2 Instance on Icelake on Aamzon Linux OS in default vpc
+# Provision EC2 Instance on Icelake on Aamzon Linux OS in default vpc. Create an EC2 key pair. Associate the
+# public key with the EC2 instance. Create the private key in the local system where terraform apply is
+# done. Create a new scurity group to open up the SSH port 22 to a specific IP CIDR block
 
-  resource "random_id" "rid" {
-    byte_length = 5
+######### PLEASE NOTE TO CHANGE THE IP CIDR BLOCK TO ALLOW SSH FROM YOUR OWN ALLOWED IP ADDRESS FOR SSH #########
+
+resource "random_id" "rid" {
+  byte_length = 5
+}
+
+# RSA key of size 4096 bits
+resource "tls_private_key" "rsa" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "aws_key_pair" "TF_key" {
+  key_name   = "TF_key"
+  public_key = tls_private_key.rsa.public_key_openssh
+}
+
+resource "local_file" "TF_public_key" {
+  content  = tls_private_key.rsa.private_key_pem
+  filename = "tfkey"
+}
+
+resource "aws_security_group" "ssh_security_group" {
+  description = "security group to configure ports for ssh"
+  ingress {
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+
+    ## CHANGE THE IP CIDR BLOCK BELOW TO ALL YOUR OWN SSH PORT ##
+    #cidr_blocks = ["a.b.c.d/x"]
+    cidr_blocks = ["136.52.34.139/32"]
   }
+}
+
+resource "aws_network_interface_sg_attachment" "sg_attachment" {
+  security_group_id    = aws_security_group.ssh_security_group.id
+  network_interface_id = module.ec2-vm.primary_network_interface_id
+}
 
 module "ec2-vm" {
-  source = "../../"
+  source   = "../../"
+  key_name = "TF_key"
   tags = {
     Name     = "my-test-vm-${random_id.rid.dec}"
     Owner    = "OwnerName-${random_id.rid.dec}",
