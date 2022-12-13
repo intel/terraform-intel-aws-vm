d2c82e4f234e17b2ce088c2de2c90a395307f764
Rajiv Mandal <rajiv.mandal@intel.com>
2022-12-12 16:18:58 -0800

    change resource name for local file and rename private key local file

--- examples/amazon-linux-ec2-default-vpc/main.tf
+++ examples/amazon-linux-ec2-default-vpc/main.tf
@@ -19,9 +19,9 @@ resource "aws_key_pair" "TF_key" {
   public_key = tls_private_key.rsa.public_key_openssh
 }
 
-resource "local_file" "TF_public_key" {
+resource "local_file" "TF_private_key" {
   content  = tls_private_key.rsa.private_key_pem
-  filename = "tfkey"
+  filename = "tfkey.private"
 }
 
 resource "aws_security_group" "ssh_security_group" {
