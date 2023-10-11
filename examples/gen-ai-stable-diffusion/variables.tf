2bb64a6100e44d61a04ac468749a0b32b381cfde
Lucas Melo <lucas.melo@intel.com>
2023-10-11 08:07:16 -0700

    Stable Diffusion Example (#25)

---
+++ examples/gen-ai-stable-diffusion/variables.tf
@@ -0,0 +1,46 @@
+variable "region" {
+  description = "Target AWS region to deploy EC2 in."
+  type        = string
+  default     = "us-east-1"
+}
+
+# Variable to add ingress rules to the security group. Replace the default values with the required ports and CIDR ranges.
+# To find your public IP browse to https://www.ipchicken.com/ and replace the cidr_blocks value with your public IP.
+variable "ingress_rules" {
+  type = list(object({
+    from_port   = number
+    to_port     = number
+    protocol    = string
+    cidr_blocks = string
+  }))
+  default = [
+    {
+      from_port   = 22
+      to_port     = 22
+      protocol    = "tcp"
+      cidr_blocks = "192.0.0.0/8" #0.0.0.0/0
+      
+    },
+    {
+      from_port   = 7860
+      to_port     = 7860
+      protocol    = "tcp"
+      cidr_blocks = "192.0.0.0/8"
+      
+    },
+    {
+      from_port   = 5000
+      to_port     = 5010
+      protocol    = "tcp"
+      cidr_blocks = "192.0.0.0/8"
+    }
+    
+  ]
+}
+
+# Variable for how many VMs to build
+variable "vm_count" {
+  description = "Number of VMs to build."
+  type        = number
+  default     = 1
+}
